ebb776dac32bc17d9b62a3e6ff0c3c3b4d824b5b
rissol86 <119512061+rissol86@users.noreply.github.com>
2023-11-18 14:00:38 +0300

    sdfsf
    ываыв

---
+++ app.js
@@ -0,0 +1,62 @@
+let model = document.querySelector (".knopka-two-slide")
+
+let a = document.querySelector (".container-roditel")
+
+let b = document.querySelector (".container-2")
+
+
+
+let t = document.querySelector (".top-menu")
+
+let y = document.querySelector (".byrger-footer")
+
+let u = document.querySelector (".menu-footer")
+
+
+function Alex_foot(){
+    u.style.display="block"
+}
+
+
+
+
+
+model.addEventListener("click",function(){
+    a.style.display="flex"
+})
+
+
+
+model.addEventListener("click",function(){
+    a.style.display="flex"
+})
+
+
+let k = document.querySelector(".close")
+
+k.addEventListener("click",function(){
+    a.style.display="none"
+})
+
+
+
+let w  = document.querySelector(".byrger")
+
+w.addEventListener("click",function(){
+    
+})
+
+let japan = document.querySelector(".top-menu")
+
+w.addEventListener("click",function(){
+
+
+    if(japan.style.display=="none"){
+        japan.style.display="block"
+    }
+
+    else{
+        japan.style.display="none"
+    }
+
+})
